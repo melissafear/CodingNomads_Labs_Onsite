1676165cd7d00158f3303efc8d7787109022c7bb
Melissa Fear <melissa.fear@gmail.com>
2019-03-10 20:54:35 +0800

    finished

--- week_03/07_variable_arguments/01_args.py
+++ week_03/07_variable_arguments/01_args.py
@@ -2,3 +2,10 @@
 Write a script with a function that demonstrates the use of *args.
 
 '''
+
+
+
+def amazing_sandwich_fillings(*fillings):
+    return print(*fillings)
+
+amazing_sandwich_fillings("bacon", "lettuce", "tomato")
--- week_03/07_variable_arguments/02_kwargs.py
+++ week_03/07_variable_arguments/02_kwargs.py
@@ -2,3 +2,9 @@
 Write a script with a function that demonstrates the use of **kwargs.
 
 '''
+
+def famous_couples(**couples):
+    for k,v in couples.items():
+        print(f"{k} and",v)
+
+famous_couples(torville = "dean", peanutbutter = "jelly", romeo = "juliet")
