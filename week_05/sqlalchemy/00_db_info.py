32b073fb37d998615c3583e89a62a9ab5b9a0a51
Melissa Fear <melissa.fear@gmail.com>
2019-03-21 19:23:54 +0800

    finished

--- week_05/sqlalchemy/00_db_info.py
+++ week_05/sqlalchemy/00_db_info.py
@@ -5,3 +5,24 @@ All of the following exercises should be done using sqlalchemy.
 Using the dvdrental schema, write the necessary code to print information about the film and category table.
 
 '''
+
+
+
+from pprint import pprint
+import sqlalchemy
+
+import sqlalchemy
+
+
+engine = sqlalchemy.create_engine(f'postgres+psycopg2://melissa@localhost:5432/dvdrental')
+connection = engine.connect()
+metadata = sqlalchemy.MetaData()
+film = sqlalchemy.Table('film', metadata, autoload=True, autoload_with=engine)
+category = sqlalchemy.Table('category', metadata, autoload=True, autoload_with=engine)
+
+
+print(film.columns.keys())
+print(repr(metadata.tables['film']))
+
+print(category.columns.keys())
+print(repr(metadata.tables['category']))
